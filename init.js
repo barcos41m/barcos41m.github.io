9b74e557a0ebb09d7c7abf94a8cabd5b1b90fe13
barcos41m <barcos41m@gmail.com>
2023-04-05 13:47:28 -0600

    cambio

---
+++ init.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyA-55B8HByIxtBRovvjLQbPa9Ci0Nll_Ks",
+    authDomain: "barcos41m-e2454.firebaseapp.com",
+    projectId: "barcos41m-e2454",
+    storageBucket: "barcos41m-e2454.appspot.com",
+    messagingSenderId: "896200305983",
+    appId: "1:896200305983:web:f72404f145a62eeff7f78a",
+    measurementId: "G-HKGN6NNF7T"
+     });
+ 
